30b1eaddf20f29af9c70045427fd08e3e1a00b0b
steamcrow <daniel@steamcrow.com>
2026-01-19 07:12:50 -0700

    Reorganize buttons and display total in UI

--- scripts/coffin_canyon_skeleton_5.js
+++ scripts/coffin_canyon_skeleton_5.js
@@ -24,11 +24,12 @@ CCFB.define("components/skeleton", function(C) {
                             <input type="text" id="roster-name" placeholder="ROSTER NAME">
 
                             <div class="top-tools ml-auto">
-                                <span id="display-total" title="Liberty Bucks">0 ₤</span>
-                                <button onclick="window.CCFB.shareRoster()" title="Share"><i class="fa fa-share-alt"></i></button>
-                                <button onclick="window.printRoster()" title="Print"><i class="fa fa-print"></i></button>
-                            </div>
-                        </div>
+                                <button onclick="window.CCFB.toggleViewMode('card')" title="Card View"><i class="fa fa-th-large"></i></button>
+                                <button onclick="window.CCFB.toggleViewMode('list')" title="List View"><i class="fa fa-list"></i></button>
+                                <span id="display-total">0 ₤</span>
+                                <button onclick="window.CCFB.shareRoster()"><i class="fa fa-share-alt"></i></button>
+                                <button onclick="window.printRoster()"><i class="fa fa-print"></i></button>
+                            </div>      
                     </div>
 
                     <div class="cc-grid">
