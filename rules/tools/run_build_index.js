3c477c24e587fb3bd1554b3e2918f3acc09d0cc7
steamcrow <daniel@steamcrow.com>
2026-01-25 17:41:13 -0700

    Update run_build_index.js

--- rules/tools/run_build_index.js
+++ rules/tools/run_build_index.js
@@ -1,14 +1,18 @@
 const fs = require("fs");
 const path = require("path");
-
-const rulesPath = path.resolve(__dirname, "../rules_master.json");
-const outPath = path.resolve(__dirname, "../rules_index.json");
-
-const rules = JSON.parse(fs.readFileSync(rulesPath, "utf8"));
 const { buildIndex } = require("./build_rules_index");
 
-const index = buildIndex(rules);
+// INPUT: rules_base.json (source of truth)
+const rulesPath = path.join(__dirname, "..", "rules_base.json");
+const rulesBase = JSON.parse(fs.readFileSync(rulesPath, "utf8"));
 
+// OUTPUT: rules_index.json
+const outPath = path.join(__dirname, "..", "rules_index.json");
+
+// Build index
+const index = buildIndex(rulesBase);
+
+// Write index
 fs.writeFileSync(outPath, JSON.stringify(index, null, 2));
 
 console.log("✅ Rules index rebuilt");
