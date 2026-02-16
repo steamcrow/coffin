56c5da4805ec440cb2cb29ca23f7795cbf2205f6
steamcrow <daniel@steamcrow.com>
2026-02-15 18:41:01 -0700

    Increase lensZoomOffset from 1.25 to 1.75

--- rules/apps/canyon_map/cc_canyon_map_app.js
+++ rules/apps/canyon_map/cc_canyon_map_app.js
@@ -28,7 +28,7 @@
       "https://raw.githubusercontent.com/steamcrow/coffin/main/rules/vendor/leaflet/leaflet.js",
 
     lensEnabled: true,
-    lensZoomOffset: 1.25,
+    lensZoomOffset: 1.75,
 
     lockHorizontalPan: false,
     maxHorizontalDriftPx: 260,
