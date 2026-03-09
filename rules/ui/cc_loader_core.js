21d37f5231d2f04c1062cd01539facec1cdbaca0
steamcrow <daniel@steamcrow.com>
2026-03-08 17:27:15 -0700

    Update cc_loader_core.js

--- rules/ui/cc_loader_core.js
+++ rules/ui/cc_loader_core.js
@@ -137,7 +137,7 @@ window.addEventListener('error', function(e) {
       title:       'Scenario Builder',
       icon:        'fa-map-signs',
       description: 'Generate scenarios',
-      file:        'cc_app_scenario_builder.js?v=13',
+      file:        'cc_app_scenario_builder.js?v=14',
       helpTitle:   'Scenario Builder',
       helpBody: [
         'Generate a full game scenario: location, objectives, monster pressure, noise threshold, and a narrative hook to set the scene.',
